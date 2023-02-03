0cea4894f9293ddf474a67e1b601ee495c0e7afa
Caden Wilcox <wilcox4357@gmail.com>
2023-02-02 18:58:56 -0800

    wk4 day 3

--- week4/day2/OH/algo.js
+++ week4/day2/OH/algo.js
@@ -28,3 +28,73 @@ function breakingRecords(scores){
 }
 console.log(breakingRecords([10, 5, 20, 20, 4, 5, 2, 25, 1]))
 console.log(breakingRecords([3, 4, 21, 36, 10, 28, 35, 5, 24, 42]))
+
+
+
+
+// Given a string return an object with the count of every character in the string 
+// example charCount('hello') would return {h:1, e:1, l:2, o:1}
+// * Write a function that takes a string as a param
+// * we need an object that holds the count of each letter in the string to return 
+// * write a for loop to loop through the string
+// * if the letter we are on exists in the result we need to increase the value associated with that key by 1 
+// * If it doesnt exist then we need to add it to result and set the value to one 
+// ! {h:1, e:1, l:2, o:1}
+function charCount(str){
+    let result = {} // {h:1, e:1, l:2, o:1}
+    for(let i = 0; i < str.length; i++){
+        let char = str[i] // o
+        console.log('CHAR', char);
+        if(result[char]){ //result[o]
+            result[char] = result[char] + 1
+        }else{
+            result[char] = 1 // result[o] = 1
+        }
+    }
+    console.log(result);
+}
+charCount('hello')
+
+
+// if(result['h']){
+//     // console.log('H is in the object');
+//     result['h']++
+// }else{
+//     result['z'] = 1
+// }
+
+
+
+
+// let result = {}
+
+// result['h'] = 1
+// result['e'] = 1
+// result['l'] = 2
+// result['o'] = 1
+
+
+// console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Reverse an array 
+// example reverseArr([1,2,3,4]) would return [4,3,2,1]
