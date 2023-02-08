f586a533700a9ad5e27505b6972e04a289a4e0f2
Caden Wilcox <wilcox4357@gmail.com>
2023-02-08 12:41:29 -0800

    change

--- week4/day2/OH/algo.js
+++ week4/day2/OH/algo.js
@@ -33,7 +33,7 @@ console.log(breakingRecords([3, 4, 21, 36, 10, 28, 35, 5, 24, 42]))
 // Itterate over an object
 let obj = {a:1, b:2, c:'C'}
 for (let key in obj){
-    console.log(obj[key])
+    console.log('Key: ',key,'Value: ',obj[key])
 }
 
 
@@ -46,8 +46,13 @@ let coordinate = {x:2, y:1, xy:9}
 // Check if x+y = 3
 console.log('X', coordinate['x'])
 console.log('Y', coordinate['y'])
-//           2                   1
+
+// Adds together the values at the two keys, they happen to be numbers so the sum will = 3
+//                       2                 1
 // let sum = coordinate['x'] + coordinate['y']
+
+
+// this will not work it will concatenate the 2 strings ('x', 'y') so it will look for a key of 'xy' in the coordinate object 
 //           coordinate['xy']
 let sum = coordinate['x' + 'y']
 
@@ -59,12 +64,28 @@ let coordinates = [
     {x:1, y:2},
     {x:3, y:1},
     {x:4, y:6},
-    {x:0, y:4},
+    {x:0, y:3},
 ]
 // * Given the list of coordinates above determine if x+y = n 
 // * return back a number of how many times x+y = n 
-
-
+let x = null
+let y = null
+let count = 0
+for (let idx in coordinates){
+    for (let key in coordinates[idx]){
+        // console.log(coordinates[idx][key] )
+        if (key === 'x'){
+            x = coordinates[idx][key]
+        }
+        else if(key === 'y'){
+            y = coordinates[idx][key]
+        }
+    }
+    if (x+y === 3){
+        count++
+    }
+}
+console.log(count)
 
 // Given a string return an object with the count of every character in the string 
 // example charCount('hello') would return {h:1, e:1, l:2, o:1}
